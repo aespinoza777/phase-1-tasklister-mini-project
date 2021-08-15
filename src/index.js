de32e312baea1f370cfe0bb152826ed6823ff752
AntonioEsp <github email address>
2021-08-14 22:12:09 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  
-  document.getElementById("create-task-form");
-  document.getElementById("new-task-description");
-  document.getElementById("new-task-priority");
 
+  let createTask = document.getElementById("create-task-form");
+  let newTask = document.getElementById("new-task-description");
+  let newTaskPriority = document.getElementById("new-task-priority");
+  let tasks = document.getElementById("tasks")
+
+  createTask.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    console.log(newTask.value)
+    console.log(tasks)
+    //create li
+    let node = document.createElement('li');
+    //set li text to newTask.value
+
+    
+    //create button <button data-description="shower">X</button>
+    //set button text to X 
+    // set data-description to newTask.value
+    // add button to li
+
+    //add li to "tasks"
+  })
 });
+// document.getElementById('textbox_id').value
